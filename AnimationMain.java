9f74566b2ddbe2d3e8d7c068964f11de2d6d21e1
Michael Harwood <mk_gecko@yahoo.com>
2015-12-29 18:01:33 -0500

    Step 2: making a ball move. I also make it collide with the walls. I'm using a Thread.sleep in an infinite loop for the animation. There are other ways of doing this too: using a TimerTask or using at Timer object and ActionListener.

--- AnimationMain.java
+++ AnimationMain.java
@@ -9,29 +9,80 @@ public class AnimationMain {
 	public static void main(String[] args) {
 		new AnimationMain();
 	}
-	
+
+	/***** Constants *****/
+	static final int SLEEPTIME = 10;
+	static final int GRWIDTH = 800;
+	static final int GRHEIGHT = 600;
+
 	/***** Global (instance) Variables ******/
-	GraphicsConsole gc = new GraphicsConsole (800,600);
-	int ballx, bally;
-	int ballSize = 30;
-	
+	GraphicsConsole gc = new GraphicsConsole (GRWIDTH, GRHEIGHT);
+	Ball ball;
+	int lives = 4;
+	boolean isPlaying = true;
+
 	/****** Constructor ********/
 	AnimationMain() {
-		initialize();	//ie. setup.
-		drawGraphics();
+		initialize();
+
+		//main game loop
+		while (gc.getKeyCode()  != 'Q' && isPlaying) {		//press q to quit
+
+			moveBall();
+			//movePaddle();
+			//checkCollisions();
+			drawGraphics();
+
+			gc.sleep(SLEEPTIME);
+
+			if (lives <= 0) isPlaying = false;
+		}
+		gc.drawString("GAME OVER", 30, 30);
 	}
-	
+
 	/****** Methods for game *******/	
 	void initialize() {
-		ballx = gc.getDrawWidth()/2;
-		bally = 100;
+
 		gc.setAntiAlias(true);
 		gc.setBackgroundColor(Color.BLACK);
-		gc.clear();	//this must be done for the background colour to take effect
+
+		ball = new Ball(GRWIDTH, 100); //the ball will start somewhere in this section of the screen (but at least 75 pixels from the edges of this region)
+	}
+
+	void moveBall() {
+		ball.x += ball.xspeed;
+		ball.y += ball.yspeed;
+		
+		//bounce off bottom of screen		
+		if (ball.y > gc.getDrawHeight()) {
+			ball.yspeed *=-1;
+			lives--;
+			ball.colour = new Color(Color.HSBtoRGB((float)Math.random(), 1.0f, 1.0f));
+		}
+		
+		//right side of screen
+		if (ball.x > gc.getDrawWidth()) {
+			ball.xspeed *=-1;
+		}
+		//top of screen
+		if (ball.y < 0) {
+			ball.yspeed *=-1;
+			ball.yspeed++;			
+		}
+		//left side of screen
+		if (ball.x < 0) {
+			ball.xspeed *=-1;
+		}
 	}
 	
 	void drawGraphics() {
-		gc.setColor(Color.RED);
-		gc.fillOval(ballx, bally, ballSize, ballSize);
+		//clear screen and redraw everything
+		synchronized(gc) {
+			gc.clear();		
+			gc.setColor(Color.WHITE);
+			gc.drawString("LIVES = " + lives, 30, 70);
+			gc.setColor(ball.colour);
+			gc.fillOval(ball.x, ball.y, ball.width, ball.height);
+		}
 	}
 }
--- README.md
+++ README.md
@@ -8,7 +8,26 @@ This is done using the HSA GraphicsConsole so that you don't have to learn Java 
 I'm making "commits" in Git/GitHub for each major improvement. 
 Please start at the first one, and go through them so that you understand what is happening.
 
-##1. Drawing a circle, using variables for the location of it.
+##2. Making a ball move. Making it bounce off the edges of the screen
 
-* This is pretty much straight forward.
-* I've put the program into two separate methods because we'll be using this way of doing things later.
+* The ball is a separate object (with a separate class Ball.java). This is because the ball has properties that it should maintain
+and that we want to change. We can also create multiple balls this way.
+* Notice some new global variables: lives, isPlaying (this will be used to end the game)
+* Notice some constants. These make it easy to change numbers in the code without having to do search and replace.
+* A **game loop** has been added. This is a standard way of doing things. 
+* The sleep() function is ESSENTIAL. This allows the computer to do other things. It also varies the speed of the game
+  * ball.xspeed and ball.yspeed are how many pixels the ball moves each time. If this number is larger, the ball moves faster. 
+  However, it also becomes more jerky because it actually jumps this number of pixels.
+  * So, keep the xspeed small, and vary the game speed by changing SLEEPTIME. It is in milliseconds
+* Interesting, both a capital or lowercase Q will end the game.
+
+  ---
+ 
+ Okay, I've also made it so that when the ball hits the bottom of the screen, you lose a life and the ball changes colour.
+ 
+ ##Next time:
+ 
+ :one: I'm going to make a nicer font in the next release so that the text looks better.
+ :two: Notice how the ball actually goes off the screen on the right and bottom. This needs fixing.
+ :three: Let's draw a rectangle to make sure that the ball actually starts in the place that we want it to.
+ 
